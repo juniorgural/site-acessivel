fd4b6ae44b0052eb6319350022c363ecd7a88097
juniorgural <137784826+juniorgural@users.noreply.github.com>
2025-02-19 10:00:31 -0300

    ok

--- script.js
+++ script.js
@@ -22,7 +22,7 @@ document.addEventListener('DOMContentLoaded', function(){
          tamanhoAtualFonte += 0.1;
          document.body.style.fontSize = `${tamanhoAtualFonte}rem`
  
-     })
+     }) 
  
      diminuiFonteBotao.addEventListener('click', function(){
          tamanhoAtualFonte -= 0.1;
